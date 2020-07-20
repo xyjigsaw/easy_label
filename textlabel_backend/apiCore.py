51c8077c7403a72b319cc7f461ffed306640eb49
Reacubeth <644327005@qq.com>
2020-07-20 17:49:59 +0800

    backend added

---
+++ textlabel_backend/apiCore.py
@@ -0,0 +1,291 @@
+# Name: apiCore
+# Author: Reacubeth
+# Time: 2020/6/22 21:45
+# Mail: noverfitting@gmail.com
+# Site: www.omegaxyz.com
+# *_*coding:utf-8 *_*
+
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import time
+import os
+import uuid
+
+from db_toolkit import db_get_project, db_insert_project, db_delete_project, db_insert_file, \
+    db_get_entity_class, db_update_entity_class, db_delete_entity_class, db_fetch_file_limit, \
+    db_update_entity_list, db_insert_entity_class, db_change_status
+from toolkit.pdf_parser import Parser
+from readXML import PaperXML
+from wsCore import users, routes
+
+
+app = FastAPI(routes=routes)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "http://10.10.10.1:8082",
+    "http://10.10.10.1:8081",
+    "http://10.10.10.1:8080",
+    "http:192.169.0.3:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#############################################
+# Basic
+#############################################
+
+
+class ProjectItem(BaseModel):
+    p_id: str = ''
+    name: str = ''
+
+
+@app.post('/fetch_project')
+async def fetch_project(request: ProjectItem):
+    start = time.time()
+    api_data = db_get_project()
+    print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Fetch Project Success')
+    return {"message": "success", 'time': time.time() - start, 'data': api_data}
+
+
+class ClassItem4project(BaseModel):
+    p_id: str = ''
+
+
+@app.post('/fetch_class')
+async def fetch_class(request: ClassItem4project):
+    start = time.time()
+    p_id = request.p_id
+    api_data = db_get_entity_class(p_id)
+    print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Fetch Entity_class Success')
+    return {"message": "success", 'time': time.time() - start, 'data': api_data}
+
+
+class ClassItem(BaseModel):
+    addLabelName: str = ''
+    addLabelColor: str = ''
+    addLabelDes: str = ''
+    p_id: str = ''
+
+
+@app.post('/add_class')
+async def add_class(request: ClassItem):
+    start = time.time()
+    try:
+        addLabelName = request.addLabelName
+        addLabelColor = request.addLabelColor
+        addLabelDes = request.addLabelDes
+        p_id = request.p_id
+        info = db_insert_entity_class(addLabelName, addLabelColor, addLabelDes, p_id)
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Add Entity_class Success')
+        return {"message": info, 'time': time.time() - start, 'data': ''}
+    except Exception as e:
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Add Entity_class Error')
+        return {"message": str(e), 'time': time.time() - start, 'data': ''}
+
+
+class UpdateClassItem(BaseModel):
+    rLabelName: str = ''
+    rLabelColor: str = ''
+    rLabelDes: str = ''
+    c_id: str = ''
+
+
+@app.post('/update_class')
+async def update_class(request: UpdateClassItem):
+    start = time.time()
+    try:
+        rLabelName = request.rLabelName
+        rLabelColor = request.rLabelColor
+        rLabelDes = request.rLabelDes
+        c_id = request.c_id
+        info = db_update_entity_class(rLabelName, rLabelColor, rLabelDes, c_id)
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Update Entity_class Success')
+        return {"message": info, 'time': time.time() - start, 'data': ''}
+    except Exception as e:
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Update Entity_class Error')
+        return {"message": str(e), 'time': time.time() - start, 'data': ''}
+
+
+class DeleteClassItem(BaseModel):
+    c_id: str = ''
+
+
+@app.post('/delete_class')
+async def delete_class(request: DeleteClassItem):
+    start = time.time()
+    try:
+        c_id = request.c_id
+        info = db_delete_entity_class(c_id)
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Delete Entity_class Success')
+        return {"message": info, 'time': time.time() - start, 'data': ''}
+    except Exception as e:
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Delete Entity_class Error')
+        return {"message": str(e), 'time': time.time() - start, 'data': ''}
+
+
+class DeleteProjectItem(BaseModel):
+    p_id: str = ''
+    path: str = ''
+
+
+@app.post('/delete_project')
+async def delete_class(request: DeleteProjectItem):
+    start = time.time()
+    try:
+        p_id = request.p_id
+        path = request.path
+        info = db_delete_project(p_id)
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Delete Project Success')
+        return {"message": info, 'time': time.time() - start, 'data': ''}
+    except Exception as e:
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Delete Project Error')
+        return {"message": str(e), 'time': time.time() - start, 'data': ''}
+
+
+class FileItem(BaseModel):
+    p_id: str = ''
+    anchor: int = 0
+    pageSize: int = None
+
+
+@app.post('/fetch_file')
+async def fetch_file(request: FileItem):
+    start = time.time()
+    p_id = request.p_id
+    anchor = request.anchor
+    pageSize = request.pageSize
+    api_data = db_fetch_file_limit(p_id, anchor, pageSize)
+    print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Fetch File Success')
+    return {"message": "success", 'time': time.time() - start, 'data': api_data}
+
+
+class SubmitFileItem(BaseModel):
+    f_id: str = ''
+    entity_list: list = []
+
+
+@app.post('/submit_entity_list')
+async def submit_entity_list(request: SubmitFileItem):
+    start = time.time()
+    try:
+        f_id = request.f_id
+        entity_list = request.entity_list
+        info = db_update_entity_list(f_id, entity_list)
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Submit Entity_list Success')
+        return {"message": info, 'time': time.time() - start, 'data': ''}
+    except Exception:
+        return {"message": "error", 'time': time.time() - start, 'data': ''}
+
+
+class StatusItem(BaseModel):
+    f_id: str = ''
+    status: int = None
+
+
+@app.post('/change_status')
+async def change_status(request: StatusItem):
+    start = time.time()
+    try:
+        f_id = request.f_id
+        status = request.status
+        info = db_change_status(f_id, status)
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'File Status Change Success')
+        return {"message": info, 'time': time.time() - start, 'data': ''}
+    except Exception:
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'File Status Change Error')
+        return {"message": "error", 'time': time.time() - start, 'data': ''}
+
+
+#############################################
+# Upload
+#############################################
+
+
+@app.post("/file_upload")
+async def file_upload(file: UploadFile = File(...)):
+    start = time.time()
+    try:
+        res = await file.read()
+        with open('upload/' + file.filename, "wb") as f:
+            f.write(res)
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'File Upload Success')
+        return {"message": "success", 'time': time.time() - start, 'filepath': file.filename}
+    except Exception as e:
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'File Upload Error')
+        return {"message": str(e), 'time': time.time() - start, 'filepath': file.filename}
+
+
+def findAllFile(base):
+    for root, ds, fs in os.walk(base):
+        for f in fs:
+            if f.endswith('.pdf'):
+                fullname = os.path.join(root, f)
+                yield fullname
+
+
+class ZIPItem(BaseModel):
+    filePath: str = None
+    addProjectName: str = None
+
+
+@app.post('/unzip')
+async def unzip(request: ZIPItem):
+    start = time.time()
+    filePath = request.filePath
+    addProjectName = request.addProjectName
+    # unzip upload/test.zip -d upload/xx
+    cmd = 'unzip -o upload/' + filePath + ' -d upload/' + addProjectName
+    os.system(cmd)
+
+    base = 'upload\\' + addProjectName
+    parser = Parser('cermine')
+    output_texts_ls = []
+    for i in findAllFile(base):
+        if '__MACOSX' not in i:
+            tmpName = i.replace('\\', '/')
+            print(tmpName)
+            try:
+                parser.parse('text', tmpName, 'upload/' + addProjectName + '/parse', 50)
+                paper = PaperXML('upload/' + addProjectName + '/parse/' + tmpName[tmpName.rfind('/') + 1:-3] + 'cermine.xml')
+                texts = paper.get_secs()
+                output_texts_ls.append({'texts': texts, 'name': tmpName[tmpName.rfind('/') + 1:-4], 'path': tmpName})
+            except Exception as e:
+                pass
+
+    p_id = str(uuid.uuid4())
+    p_id = ''.join(p_id.split('-'))
+    if len(output_texts_ls) == 0:
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Unzip Error No Papers in zip')
+        return {"message": 'No Papers in zip', 'time': time.time() - start, 'data': ''}
+    try:
+        db_insert_project(p_id, 'upload/' + addProjectName, addProjectName, len(output_texts_ls))
+        for item in output_texts_ls:
+            try:
+                db_insert_file(item['name'], item['path'], item['texts'], p_id)
+            except Exception as e:
+                pass
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Unzip Success')
+        return {"message": "success", 'time': time.time() - start, 'data': ''}
+    except Exception as e:
+        print(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())), 'Unzip Error')
+        return {"message": str(e), 'time': time.time() - start, 'data': ''}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app=app, host="127.0.0.1", port=8000, workers=1)
+
+
+# uvicorn apiCore:app --reload --port 8000 --host 0.0.0.0
+# pip install python-multipart
---
+++ textlabel_backend/wsCore.py
@@ -0,0 +1,87 @@
+# Name: wsCore
+# Author: Reacubeth
+# Time: 2020/7/15 18:40
+# Mail: noverfitting@gmail.com
+# Site: www.omegaxyz.com
+# *_*coding:utf-8 *_*
+
+from starlette.endpoints import WebSocketEndpoint
+from starlette.routing import WebSocketRoute
+import json
+
+users = {}
+p_num = {}
+
+
+class WSEndPoint(WebSocketEndpoint):
+    @staticmethod
+    async def get_socket_ID(websocket):
+        socket_str = str(websocket)[1:-1]
+        socket_list = socket_str.split(' ')
+        socket_ID = socket_list[3]
+        return socket_ID
+
+    @staticmethod
+    async def ws_exit(p_id, socket_ID):
+        users.pop(socket_ID)
+        print(socket_ID + ' Exit!')
+        if p_id in p_num:
+            p_num[p_id] -= 1
+        for user in users:
+            info = {'subject': 'total', 'message': {'num': len(users),
+                                                    'p_id': p_id,
+                                                    'socket_ID': socket_ID,
+                                                    'des': 'off'}}
+            if users[user]['p_id'] == p_id:
+                await users[user]['websocket'].send_text(json.dumps(info))
+
+    # Connect
+    async def on_connect(self, websocket):
+        await websocket.accept()
+        socket_ID = await self.get_socket_ID(websocket)
+        openMsg = await websocket.receive_text()
+        p_id = json.loads(openMsg)['message']['p_id']
+        users[socket_ID] = {'name': socket_ID, 'websocket': websocket, 'p_id': p_id}
+        if p_id not in p_num:
+            p_num[p_id] = 1
+        else:
+            p_num[p_id] += 1
+
+        # broadcast entry
+        for user in users:
+            info = {'subject': 'total',
+                    'message': {'num': p_num[users[user]['p_id']],
+                                'p_id': users[socket_ID]['p_id'],
+                                'socket_ID': socket_ID,
+                                'des': 'on'}}
+            if p_id == users[user]['p_id']:
+                await users[user]['websocket'].send_text(json.dumps(info))
+
+    # Broadcast Info
+    async def on_receive(self, websocket, data):
+        socket_ID = await self.get_socket_ID(websocket)
+        data = json.loads(data)
+        subject = data['subject']
+        message = data['message']
+
+        if subject == 'offline':
+            await self.ws_exit(message['p_id'], socket_ID)
+        else:
+            for user in users:
+                info = {'subject': subject, 'message': message}
+                if users[user]['name'] != socket_ID and users[user]['p_id'] == message['p_id']:
+                    await users[user]['websocket'].send_text(json.dumps(info))
+
+    # Disconnect
+    async def on_disconnect(self, websocket, close_code):
+        socket_ID = await self.get_socket_ID(websocket)
+        try:
+            p_id = users[socket_ID]['p_id']
+            await self.ws_exit(p_id, socket_ID)
+        except Exception as e:
+            pass
+
+
+routes = [
+    WebSocketRoute("/ws", WSEndPoint)
+]
