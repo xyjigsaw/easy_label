208b57aefc678fc8782d7ae68657fe17723134a5
Reacubeth <644327005@qq.com>
2020-07-27 18:04:57 +0800

    add database structure

---
+++ label_sys.sql
@@ -0,0 +1,62 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : XY-Laptop
+ Source Server Type    : MySQL
+ Source Server Version : 80019
+ Source Host           : 192.168.0.4:3306
+ Source Schema         : label_sys
+
+ Target Server Type    : MySQL
+ Target Server Version : 80019
+ File Encoding         : 65001
+
+ Date: 27/07/2020 18:03:21
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for entity_class
+-- ----------------------------
+DROP TABLE IF EXISTS `entity_class`;
+CREATE TABLE `entity_class` (
+  `c_id` int NOT NULL AUTO_INCREMENT,
+  `label` varchar(255) NOT NULL,
+  `color` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `p_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`c_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for file
+-- ----------------------------
+DROP TABLE IF EXISTS `file`;
+CREATE TABLE `file` (
+  `f_id` int NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(255) NOT NULL,
+  `file_path` varchar(255) DEFAULT NULL,
+  `version` int unsigned NOT NULL,
+  `entity_list` json DEFAULT NULL,
+  `text` longtext,
+  `p_id` varchar(255) DEFAULT NULL,
+  `is_edit` int NOT NULL,
+  PRIMARY KEY (`f_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for project
+-- ----------------------------
+DROP TABLE IF EXISTS `project`;
+CREATE TABLE `project` (
+  `p_id` varchar(255) NOT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `total` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`p_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
